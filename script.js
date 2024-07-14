d2f37448561af0875b805b2c1647cb38bbe17c4f
AndyKhang404 <andykhang404@gmail.com>
2024-07-14 14:47:38 +0700

    Refactor grid initialization and input handling

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 var index = 0;
-
-document.getElementById('solve').addEventListener('click', function() {
-	// console.log(document.getElementById('cell-0').firstChild.value);
-	// document.getElementById('cell-80').firstChild.focus();
-});
-
-document.getElementById('clear').addEventListener('click', function() {
-	for (var i = 0; i < 81; i++) {
-		document.getElementById('' + i).value = '';
-	}
-});
-
-var allowedPatterns = /[^1-9]?$/;
+var grid = [];
+for (var i = 0; i < 81; i++) {
+	grid.push(0);
+}
+var allowedPatterns = /[^1-9 ]?$/;
 
 for (var i = 0; i < 80; i++) {
 	document.getElementById('' + i).addEventListener('focus', function(e) {
@@ -19,9 +11,31 @@ for (var i = 0; i < 80; i++) {
 	});
 	document.getElementById('' + i).addEventListener('input', function(e) {
 		this.value = this.value.replace(allowedPatterns, '');
+		if(e.inputType === 'insertText' && this.value.length >= 1) {
+			document.getElementById('' + (parseInt(this.id) + 1)).focus();
+		}
 	});
 }
 
 document.getElementById('80').addEventListener('input', function(e) {
 	this.value = this.value.replace(allowedPatterns, '');
+	if(e.inputType === 'insertText' && this.value.length >= 1) {
+		document.getElementById('solve').focus();
+	}
 })
+
+document.getElementById('solve').addEventListener('click', function() {
+	for (var i = 0; i < 81; i++) {
+		grid[i] = parseInt(document.getElementById('' + i).value);
+		if(isNaN(grid[i])) {
+			grid[i] = 0;
+		}
+	}
+	console.log(grid);
+});
+
+document.getElementById('clear').addEventListener('click', function() {
+	for (var i = 0; i < 81; i++) {
+		document.getElementById('' + i).value = '';
+	}
+});
